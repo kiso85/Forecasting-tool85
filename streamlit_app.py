4577e5a1af999ff7712c12eba6e9ae7f1538195e
kiso85 <jichu0805@163.com>
2025-10-24 15:32:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from prophet import Prophet
 import os
 import requests
 import glob  # <-- NUEVO: Para encontrar archivos en carpetas
